5edbbee44ed93cae65f529a63abf5f85f08fa144
karfaisarra <karfaisarra@gmail.com>
2023-10-19 10:27:07 +0200

    add signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule
