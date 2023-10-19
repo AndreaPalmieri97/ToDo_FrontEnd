c38948f2eb3b77515c53113edb6e37625f76cde5
AnaDeScisciolo <anastasiadescisciolo@gmail.com>
2023-10-19 17:33:23 +0200

    aggiunti componenti + chiamate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    DropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+
+const BASE_URL = "http://localhost:8080"
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
 
+export class TodoComponent implements OnInit{
+
+  todos: Todo[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Todo[]>(`${BASE_URL}/todo`).subscribe((res: Todo[]) => this.todos = res )
+  }
+
+  constructor(private http: HttpClient) {
+  }
 }
+
